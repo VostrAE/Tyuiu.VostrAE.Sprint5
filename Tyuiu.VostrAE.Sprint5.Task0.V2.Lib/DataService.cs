03b19b871a86936b9da7b0c4d554cb96adf845a4
A Vostr <Tyuiu.VostrAE@outlook.com>
2025-11-11 15:26:54 +0500

    Add project files.

---
+++ Tyuiu.VostrAE.Sprint5.Task0.V2.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+using System.IO;
+namespace Tyuiu.VostrAE.Sprint5.Task0.V2.Lib
+{
+    public class DataService : ISprint5Task0V2
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string name = "OutPutFileTask0.txt";
+            string path = Path.Combine(Path.GetTempPath(), name);
+            double y = (Math.Pow(Math.E,x)/x);
+            y =Math.Round(y,3);
+            File.WriteAllText(path, Convert.ToString(y));
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.VostrAE.Sprint5.Task0.V2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Tyuiu.VostrAE.Sprint5.Task0.V2.Lib;
+class program
+{
+    static void Main(string[] args)
+    {
+        int x = 3;
+        DataService ds = new DataService();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine(" Исходные данные:                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("x = " + x);
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine(" Результат:                                                               *");
+        Console.WriteLine("***************************************************************************");
+        string res = ds.SaveToFileTextData(x);
+        Console.WriteLine("Файл " + res);
+        Console.WriteLine("Создан! ");
+        Console.ReadKey();
+    }
+}
