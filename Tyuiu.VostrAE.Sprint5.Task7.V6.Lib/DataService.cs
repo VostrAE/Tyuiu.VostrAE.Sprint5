e549a280021aa446cc01dc6691ec7035ee0b73a4
A Vostr <Tyuiu.VostrAE@outlook.com>
2025-11-21 21:13:59 +0500

    Task7

---
+++ Tyuiu.VostrAE.Sprint5.Task7.V6.Lib/DataService.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using System.Numerics;
+using System.Text;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.VostrAE.Sprint5.Task7.V6.Lib
+{
+    public class DataService : ISprint5Task7V6
+    {
+        public string LoadDataAndSave(string path)
+        {
+            string pathSave = Path.Combine(Path.GetTempPath(), "OutPutDataFileTask7V6.txt");
+            FileInfo fileInfo = new FileInfo(pathSave);
+            bool FileExists = fileInfo.Exists;
+            if (FileExists)
+            {
+                File.Delete(pathSave);
+            }
+
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string nline = "";
+                    foreach (char z in line)
+                    {
+                        if ((z >= 'a' && z <= 'z') || (z >= 'A' && z <= 'Z'))
+                        {
+                            nline += '#';
+                        }
+                        else
+                        {
+                            nline += z;
+                        }
+                    }
+                    File.WriteAllText(pathSave, nline);
+                }
+            }
+            return pathSave;
+        }  
+    }
+}
+
--- Tyuiu.VostrAE.Sprint5.Task7.V6/Program.cs
+++ Tyuiu.VostrAE.Sprint5.Task7.V6/Program.cs
@@ -1,22 +1,21 @@
 ﻿using System;
 using System.Globalization;
-using Tyuiu.VostrAE.Sprint5.Task6.V18.Lib;
+using Tyuiu.VostrAE.Sprint5.Task7.V6.Lib;
 class Program
 {
     static void Main(string[] args)
     {
-        string path = @"C:\DataSprint5\InPutDataFileTask6V18.txt";
-
         DataService ds = new DataService();
         Console.WriteLine("***************************************************************************");
         Console.WriteLine(" Исходные данные:                                                         *");
         Console.WriteLine("***************************************************************************");
+        string path = @"C:\DataSprint5\InPutDataFileTask7V6.txt";
         Console.WriteLine("Данные находятся в файле: " + path);
         Console.WriteLine("***************************************************************************");
         Console.WriteLine(" Результат:                                                               *");
         Console.WriteLine("***************************************************************************");
-        double res = ds.LoadFromDataFile(path);
-        Console.WriteLine("Количество одноразрядных чисел в заданной строке: " + res);
+        string pathSave = ds.LoadDataAndSave(path);
+        Console.WriteLine("Находится в файле: " + pathSave);
         Console.ReadKey();
     }
 }
